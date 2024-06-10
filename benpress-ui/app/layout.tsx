b690209e8d8eccd9c0d9e5140e65c87120459d16
bkhazaal <thajukez101@gmail.com>
2024-06-10 10:38:35 -0700

    cool

--- benpress-ui/app/layout.tsx
+++ benpress-ui/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/ui/Navbar";
+import Sidebar from "@/components/ui/sidebar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -19,7 +20,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <div className="flex">
+          <div className="hidden md:block h-[100vh] w-[350px]">
+            <Sidebar />
+          </div>
+          <div className="p-5 w-full md:max-w-[1140px]"> {children}</div>
+        </div>
       </body>
     </html>
   );
