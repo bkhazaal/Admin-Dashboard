751d3bde0cd209784d63f132ec63597a16898efb
bkhazaal <thajukez101@gmail.com>
2024-06-10 11:00:32 -0700

    morechanges

--- benpress-ui/components/ui/sidebar.tsx
+++ benpress-ui/components/ui/sidebar.tsx
@@ -21,20 +21,41 @@ import Link from "next/link";
 
 const Sidebar = () => {
   return (
-    <Command>
+    <Command className="bg-secondary rounded-none">
       <CommandInput placeholder="Type a command or search..." />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Suggestions">
-          <CommandItem>Calendar</CommandItem>
-          <CommandItem>Search Emoji</CommandItem>
-          <CommandItem>Calculator</CommandItem>
+        <CommandGroup heading="Things">
+          <CommandItem>
+            <LayoutDashboard className="mr-2 h-4 2-4" />
+            <Link href="/">Dashboard</Link>
+          </CommandItem>
+          <CommandItem>
+            <Newspaper className="mr-2 h-4 2-4" />
+            <Link href="/">Posts</Link>
+          </CommandItem>
+          <CommandItem>
+            <Folders className="mr-2 h-4 2-4" />
+            <Link href="/">Folders</Link>
+          </CommandItem>
         </CommandGroup>
         <CommandSeparator />
-        <CommandGroup heading="Settings">
-          <CommandItem>Profile</CommandItem>
-          <CommandItem>Billing</CommandItem>
-          <CommandItem>Settings</CommandItem>
+        <CommandGroup heading="Other Things">
+          <CommandItem>
+            <User className="mr-2 h-4 2-4" />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard className="mr-2 h-4 2-4" />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className="mr-2 h-4 2-4" />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
         </CommandGroup>
       </CommandList>
     </Command>
