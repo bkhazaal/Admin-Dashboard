f3054533c5d9b17050faf9256b62239f7a6a19d7
bkhazaal <thajukez101@gmail.com>
2024-06-17 17:39:05 -0700

    cool

--- benpress-ui/components/ui/dashboard/dashboard-card.tsx
+++ benpress-ui/components/ui/dashboard/dashboard-card.tsx
@@ -3,16 +3,18 @@ import { Card, CardContent } from "../card";
 
 const DashboardCard = () => {
   return (
-    <Card className="bg-slate-100 dark:bg-slate-800 p-6 pb-0">
-      <CardContent>
-        <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">
-          Posts
-        </h3>
-        <div className="flex gap-5 justify-center items-center">
-          <Newspaper className="text-slate-500" size={72} />
-        </div>
-      </CardContent>
-    </Card>
+    <div>
+      <Card className="bg-slate-100 hover:scale-105 transition dark:bg-slate-800 p-6 pb-0 sm:w-full m:w-1/2 lg:w-1/4">
+        <CardContent>
+          <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">
+            Posts
+          </h3>
+          <div className="flex gap-5 justify-center items-center">
+            <Newspaper className="text-slate-500" size={52} />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
