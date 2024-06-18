045bfb947144058554f82b20564e4aa34df45e5e
bkhazaal <thajukez101@gmail.com>
2024-06-18 09:33:49 -0700

    cool

--- benpress-ui/app/page.tsx
+++ benpress-ui/app/page.tsx
@@ -1,10 +1,30 @@
 import DashboardCard from "@/components/ui/dashboard/dashboard-card";
+import { Bird, Newspaper, PersonStanding, PocketKnife } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-      <div className="">
-        <DashboardCard />
+      <div className="flex gap-5 justify-center">
+        <DashboardCard
+          title="Epic Posts"
+          initialCount={0}
+          icon={<Newspaper className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Pocket-Knifes"
+          initialCount={0}
+          icon={<PocketKnife className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Cool Birds"
+          initialCount={0}
+          icon={<Bird className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Stick Figures"
+          initialCount={0}
+          icon={<PersonStanding className="text-slate-500" size={72} />}
+        />
       </div>
     </>
   );
--- benpress-ui/components/ui/dashboard/dashboard-card.tsx
+++ benpress-ui/components/ui/dashboard/dashboard-card.tsx
@@ -1,20 +1,38 @@
-import { Newspaper } from "lucide-react";
+"use client";
+
+import { LucideIcon } from "lucide-react";
+import { useState } from "react";
 import { Card, CardContent } from "../card";
 
-const DashboardCard = () => {
+interface DashboardCardProps {
+  title: string;
+  initialCount: number;
+  icon: React.ReactElement<LucideIcon>;
+}
+
+const DashboardCard = ({ title, initialCount, icon }: DashboardCardProps) => {
+  const [count, setCount] = useState(initialCount);
+  const clickityCounterson = (clicks: number) => {
+    clicks += 1;
+    return clicks;
+  };
   return (
-    <div>
-      <Card className="bg-slate-100 hover:scale-105 transition dark:bg-slate-800 p-6 pb-0 sm:w-full m:w-1/2 lg:w-1/4">
-        <CardContent>
-          <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">
-            Posts
-          </h3>
-          <div className="flex gap-5 justify-center items-center">
-            <Newspaper className="text-slate-500" size={52} />
-          </div>
-        </CardContent>
-      </Card>
-    </div>
+    <Card
+      onClick={() => setCount(clickityCounterson(count))}
+      className="flex justify-center bg-slate-100 hover:scale-105 transition dark:bg-slate-800 m:-1/2 lg:w-1/4"
+    >
+      <CardContent className="p-10 content-center">
+        <h3 className="text-3xl text-center font-bold text-slate-500 dark:text-slate-200">
+          {title}
+        </h3>
+        <div className="flex gap-5 justify-center items-center mt-5 mb-5">
+          {icon}
+        </div>
+        <div className="text-3xl text-center font-bold text-slate-500 dark:text-slate-200">
+          {count}
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
