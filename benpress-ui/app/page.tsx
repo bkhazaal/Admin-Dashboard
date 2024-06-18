b9dd899137365256ee8deed159482291397079b7
bkhazaal <thajukez101@gmail.com>
2024-06-17 17:40:47 -0700

    cool

--- benpress-ui/app/page.tsx
+++ benpress-ui/app/page.tsx
@@ -3,7 +3,7 @@ import DashboardCard from "@/components/ui/dashboard/dashboard-card";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col md: flex-rowjustify-between gap-5 mb-5">
+      <div className="">
         <DashboardCard />
       </div>
     </>
